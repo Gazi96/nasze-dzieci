56dbb039aa6c7f5bd49887027289458802dea229
Rafal <gazi1996@o2.pl>
2017-07-24 23:41:02 +0200

    dokonczenie podstron

--- views/partnerzy.php
+++ views/partnerzy.php
@@ -3,3 +3,8 @@
     echo $this->title;
     ?>
 </div>
+
+
+<h2 class="subpage">Partnerzy</h2>
+
+<p class="subpagetext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis error distinctio praesentium nemo, quisquam dolorum doloremque qui tenetur eveniet velit at voluptatum dolore nulla? Dolores et aliquid soluta, tempora accusantium.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis error distinctio praesentium nemo, quisquam dolorum doloremque qui tenetur eveniet velit at voluptatum dolore nulla? Dolores et aliquid soluta, tempora accusantium.</p>
